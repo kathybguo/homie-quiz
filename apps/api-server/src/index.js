971334b95183cc5027f9a62aa525c72916f70e1d
kathy <kathybguo@gmail.com>
2025-11-23 15:11:38 -0800

    fix server

--- apps/api-server/src/index.js
+++ apps/api-server/src/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const { add } = require("utils"); // import here
-
 const app = express();
-const port = 5000;
+const PORT = 2000;
 
 app.get("/add", (req, res) => {
   const { a, b } = req.query;
+  console.log(`api hit with ${a} and ${b}`);
   res.json({ result: add(Number(a), Number(b)) });
 });
 
-app.listen(port, () => {
-  console.log(`API server running on http://localhost:${port}`);
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.send("heyo your end point is up");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
