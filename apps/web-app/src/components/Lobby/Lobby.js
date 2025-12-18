5eb556640a99d9be6cc06f43676a486ee0b2e1eb
kiatchy <kathybguo@gmail.com>
2025-12-18 13:56:49 -0500

    add env examples

--- apps/web-app/src/components/Lobby/Lobby.js
+++ apps/web-app/src/components/Lobby/Lobby.js
@@ -1,12 +1,11 @@
 import { useParams } from "react-router-dom";
 
 export default function Lobby() {
-  const { sessionCode } = useParams();
-  console.log("Lobby session code:", sessionCode);
+  const { code } = useParams();
   return (
     <div>
       <h1>Lobby</h1>
-      <h2>Session Code: {sessionCode}</h2>
+      <h2>Session Code: {code}</h2>
     </div>
   );
 }
