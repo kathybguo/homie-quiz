c771cb29cc3a2a79950b8aa764cfae387dc926d3
kathybguo <kathybguo@gmail.com>
2026-02-13 16:37:15 -0500

    fix cors

--- apps/api-server/src/index.js
+++ apps/api-server/src/index.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
     origin: [
       "http://localhost:3000",
       "https://localhost:3000",
-      "https://homie-quiz-frontend.onrender.com/",
+      "https://homie-quiz-frontend.onrender.com",
     ],
     methods: ["GET", "POST"],
   },
